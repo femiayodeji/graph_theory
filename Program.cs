cfc8ec3e76e74ba0c8bbd2f4ac5658dc9ad557a9
Femi Ayodeji <femidotayodeji@gmail.com>
2021-03-02 21:44:56 +0100

    Add dotnet console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace graph_theory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
