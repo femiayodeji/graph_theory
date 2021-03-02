a8b836600690b5560f50e7b65bfdb33283272ecb
Femi Ayodeji <femidotayodeji@gmail.com>
2021-03-02 22:24:03 +0100

    Create Graph class

---
+++ Graph.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+public class Graph
+{
+    private LinkedList<int>[] _graph;
+    
+    public Graph(int vertices)
+    {
+         _graph = new LinkedList<int>[vertices]; 
+         Initialize();  
+    }
+
+    private void Initialize(){
+        for (int i = 0; i < _graph.Length; i++)
+            _graph[i] = new LinkedList<int>();
+    }
+
+    public void addEdge(int u, int v)
+    {
+        _graph[u].AddLast(v);
+        _graph[v].AddLast(u);
+    }
+
+    public override string ToString()
+    {
+        string output = "";
+        for(int i = 0; i < _graph.Length; i++)
+        {
+            output += $"Adjacent list of vertex {i+1}\n Head";
+            foreach (var item in _graph[i])
+            {
+                output += $" -> {item}";                
+            }
+            output += "\n";
+        }
+        return output;
+    }
+}
